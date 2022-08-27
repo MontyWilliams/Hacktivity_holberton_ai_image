6ff0fde507e20c2b9d36562d1d1a23db9b8f693c
Montywilliams <3650@holbertonschool.com>
2022-08-27 12:46:10 -0600

    the use effect is functionaly correct but i will have to switch to component did mount because the page needs to load in order for the algorith to work

--- pages/Madlibs/[id]/index.js
+++ pages/Madlibs/[id]/index.js
@@ -1,53 +1,74 @@
-import { useRouter } from 'next/router'
-import React, { useContext, useEffect, useState } from 'react';
-import { MadContext } from '../../../components/Context/MadContext';
-import styles from '../../../styles/madIndex_styles.module.css'
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+import { MadContext } from "../../../components/Context/MadContext";
+import styles from "../../../styles/madIndex_styles.module.css";
 
-  function getIt (props) {
 
-    for(const key in props) {
-     let page = (Object.values(props))
-     return(<div className={styles.h1}>{page.map(e => {
-      return(
-        <div>
-          <h1 key={e} className={styles.box}>{e} 
-            <form>
-              <input type="text"
-              name={key}
-              style={{  width:"100px" }}
-              onChange={handleChange} />
-            </form>
-          </h1>
-        </div>
-      )
-     })}
-     </div>)
+function getIt(props) {
+  for (const key in props) {
+    let page = Object.values(props);  
+    return (
+      <div className={styles.h1}>
+        {page.map(e => {
+          // setData(data)
+          return (
+            <div>
+              <h1 key={e} className={styles.box}>
+                {e}
+                <form>
+                  <input
+                    type="text"
+                    name={key}
+                    style={{ width: "100px" }}
+                    onChange={handleChange}
+                  />
+                </form>
+              </h1>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
-  }
-
- function handleChange(event) {
-console.log(event)
-  }
-
-const madL = () => {
-  const {madLibs, setMadLibs} = useContext(MadContext);
-  const word = madLibs.wordsAnswer
-
-
-  const router = useRouter()
-  const {id} = router.query;
-  return (
-    <div >
-      <p>Succesfully got The routes Working! this is route: "{id}"</p>
-      <p className={styles.madText}>{madLibs.description}</p>
-      {/* <button onClick={getIt} >yes</button> */}
-     
-      <div>
-  
-      {getIt(word)}
-      </div> 
-    </div>
-  )
 }
 
-export default madL
+function handleChange(event) {
+  console.log(event);
+}
+
+
+const madL = () => {
+  const { madLibs, setMadLibs } = useContext(MadContext);
+  const word = madLibs.wordsAnswer;
+  const router = useRouter();
+  const { id } = router.query;
+  const [data, setData] = useState({})
+ 
+useEffect(() => {
+  let wordArr = new Set();
+  for(let key in word) {
+    wordArr.add({[word[key]]: ""})
+  }
+  setData(e => ({
+    ...Array.from(wordArr)
+  } ))
+}, );
+
+console.log(data);
+  return (
+    <div>
+      <p>
+        Succesfully got The routes Working! this is route: "{id}"
+      </p>
+      <p className={styles.madText}>
+        {madLibs.description}
+      </p>
+      {/* <button onClick={getIt} >yes</button> */}
+      <div>
+        {getIt(word)}
+      </div>
+    </div>
+  );
+};
+
+export default madL;
