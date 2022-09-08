e4b2ed6212533320331374c735250e6859f4de4b
Montywilliams <3650@holbertonschool.com>
2022-09-08 02:29:39 -0600

    functions but not exactly how it should. The nature of the api is not great for giving multiple random words with out context so its just not a great user experience i didnt bother returning the images yet

--- pages/Images/[id]/index.js
+++ pages/Images/[id]/index.js
@@ -3,9 +3,40 @@ import React, { useContext, useEffect, useState} from 'react';
 import { MadContext } from "../../../components/Context/MadContext";
 import styles from "../../../styles/madIndex_styles.module.css";
 
+
 const AiImage = () => {
   const { madLibs} = useContext(MadContext);
-  console.log(madLibs)
+  const [aiData, setAiData] = useState('')
+  console.log(aiData);
+
+  const deepai = require('deepai');
+  deepai.setApiKey('quickstart-QUdJIGlzIGNvbWluZy4uLi4K');
+
+  async function getImage() {
+    var resp = await deepai.callStandardApi("text2img", {
+            text: 'goku',
+    });
+    console.log(resp);
+}
+
+  function getwords() {
+  
+    useEffect(() => {
+      let aiDataArray = [];
+      for (const words of Array.from([madLibs.wordsanswer])) {
+        for (const val in words) {
+          aiDataArray.push(words[val]);
+          let values = aiDataArray
+          setAiData(values.join(' '));
+          console.log(values);
+        }
+      }
+      getImage()
+     
+    }, []);
+  }
+
+
 
   return (
     <div className={styles.container}>
@@ -18,6 +49,7 @@ const AiImage = () => {
       <p className={styles.text}>
         Your Image is:
       </p>
+      {getwords()}
     </div>
   );
 }
